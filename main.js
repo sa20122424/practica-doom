e658e7ade783559e40266721d986c6600727f730
sa20122424 <saloxiddin201224@gmail.com>
2024-08-17 14:24:40 +0500

    salox

---
+++ main.js
@@ -0,0 +1,6 @@
+let lorem = document.querySelector('.p-lorem')
+let baground = prompt("backround-color kiriting")
+let color = prompt("color kiriting")
+
+lorem.style. backgroundColor = `${baground}`
+lorem.style.color = `${color}`
